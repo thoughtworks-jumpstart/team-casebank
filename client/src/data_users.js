33403d1cc2f4d1cd2019509dd1855f873115e453
lootster <sheldonkhlim@gmail.com>
2018-11-12 14:28:31 +0800

    #41 create seed data

---
+++ client/src/data_users.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    _id: "asd2390384jkbudis",
+    name: "John Mayer",
+    role: "Developer",
+    techstack: ["javascript", "python"],
+    projects: ["sleep-app", "pokemo-app", "to-do-list"]
+  },
+  {
+    _id: "asd2348923jkbudis",
+    name: "Mary Lamb",
+    role: "Developer",
+    techstack: ["javascript"],
+    projects: ["sleep-app"]
+  },
+  {
+    _id: "asd8976923jkbquds",
+    name: "Vlad",
+    role: "Developer",
+    techstack: ["python"],
+    projects: ["SIA"]
+  },
+  {
+    _id: "arh8983923jkjsigs",
+    name: "James",
+    role: "Developer",
+    techstack: ["PHP"],
+    projects: ["SIA"]
+  },
+  {
+    _id: "asq1256923osufuds",
+    name: "Daniel",
+    role: "Developer",
+    techstack: ["Java"],
+    projects: ["Freshfood"]
+  }
+ ];
+ 
