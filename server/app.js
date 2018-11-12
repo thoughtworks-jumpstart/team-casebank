fbb328138ddf8317788c3f79bc6569dc63ae8569
lootster <sheldonkhlim@gmail.com>
2018-11-12 12:03:31 +0800

    #29 #39 #40 Add initial project structure

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+require('dotenv').config();
+
+const isMongooseConnectionProvided = process.env.NODE_ENV === "integration";
+
+if (!isMongooseConnectionProvided) {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  });
+  mongoose.set('useCreateIndex', true);
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("Database is connected");
+  });
+
+  db.on("error", error => {
+    console.error("An error has occur", error);
+  });
+
+}
+
+const staticFiles = express.static(path.join(__dirname, "../client/build"));
+app.use(staticFiles);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to casebank project"
+  })
+})
+
+
+module.exports = app;
