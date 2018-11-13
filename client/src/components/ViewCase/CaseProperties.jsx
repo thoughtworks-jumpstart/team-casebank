77b54c4de2c60f4b8e4c9a8c2b36610b5464eed9
Brian <tjtbrian@gmail.com>
2018-11-13 18:31:22 +0800

    [Brian] #9 Properties can be an array and it will render each element

--- client/src/components/ViewCase/CaseProperties.jsx
+++ client/src/components/ViewCase/CaseProperties.jsx
@@ -6,9 +6,20 @@ export default function CaseProperties({ properties }) {
     array.push(
       <div key={property}>
         <h6 className="text-capitalize font-weight-bold">{property}</h6>
-        <h6 className="text-danger font-weight-normal text-capitalize">
-          {properties[property]}
-        </h6>
+        {Array.isArray(properties[property]) ? (
+          properties[property].map((string, index) => (
+            <h6
+              className="text-danger font-weight-normal text-capitalize"
+              key="index"
+            >
+              {string}
+            </h6>
+          ))
+        ) : (
+          <h6 className="text-danger font-weight-normal text-capitalize">
+            {properties[property]}
+          </h6>
+        )}
       </div>
     );
   }
