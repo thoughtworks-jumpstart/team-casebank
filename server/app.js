0e458007ff3d66f0fb4fb27571c4ae0f6615534e
Brian <tjtbrian@gmail.com>
2018-11-15 14:05:04 +0800

    [Brian] changed app route

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/cases", caseRouter);
 
-app.get("*", function(req, res) {
+app.get("/**", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
