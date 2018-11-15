500dee95331b7aadcdaa366e0cf6294d649baa9a
Angeline <atan@thoughtworks.com>
2018-11-15 16:03:09 +0800

    [Xinfang/Angeline] #55 Test display process env to navbar

--- client/src/utils/toggle.js
+++ client/src/utils/toggle.js
@@ -1,4 +1,10 @@
+import React from "react";
+
 //Toggle element off if current env is production
 export function toggle(element) {
-  return process.env.NODE_ENV.toLowerCase() !== "production" ? element : null;
+  return process.env.NODE_ENV.toLowerCase() === "production" ? (
+    element
+  ) : (
+    <div>{process.env.NODE_ENV}</div>
+  );
 }
