5d179ca3d4f5e17ab15986d69f534a09767bc88d
Axeaian <xf.chia@outlook.com>
2018-11-15 16:18:25 +0800

    [Angeline/XinFang] #23 update techstack test-cases

--- client/src/components/Search/SearchTechStack.test.js
+++ client/src/components/Search/SearchTechStack.test.js
@@ -1,14 +1,16 @@
 import { render, fireEvent } from "react-testing-library";
 import React from "react";
 import SearchTechStack from "./SearchTechStack";
+import { getTechstack } from "../../data/techStackService";
 
 describe("Renders dropdown menu for techs", () => {
   it("should render all techs that it gets from getTechStack()", () => {
     const { container } = render(<SearchTechStack />);
     const allItems = container.querySelectorAll(".dropdown-item");
     const itemValues = Array.from(allItems).map(item => item.textContent);
+    let numTech = getTechstack().length + 1;
 
-    expect(allItems.length).toBe(16);
+    expect(allItems.length).toBe(numTech);
     expect(itemValues).toContain("NoSQL");
     expect(itemValues).toContain("Javascript");
     expect(itemValues).toContain("All");
@@ -16,13 +18,14 @@ describe("Renders dropdown menu for techs", () => {
 
   it("should call prop onClick and pass in tech stack value when selected", () => {
     const handleSubmit = jest.fn();
+    let Tech = ["All", ...getTechstack().map(tech => tech)];
     const { container } = render(<SearchTechStack onClick={handleSubmit} />);
 
-    const secondItem = container.querySelectorAll(".dropdown-item")[1];
+    const secondItem = container.querySelectorAll(".dropdown-item")[2];
     fireEvent.click(secondItem);
 
     expect(handleSubmit).toHaveBeenCalledTimes(1);
-    expect(handleSubmit).toHaveBeenCalledWith("C");
+    expect(handleSubmit).toHaveBeenCalledWith(Tech[2]);
   });
 
   it("should display 'Techstack: All' on first load", () => {
