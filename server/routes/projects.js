1b4a8109cfe646ac2921fc3a68c873d01a1353c7
Brian <tjtbrian@gmail.com>
2018-11-19 16:29:14 +0800

    [Brian/Xinfang] #50 Updated routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const Project = require("../schemas/Project");
 // Cases API
 
+router.get("/:projectId", async (req, res) => {
+  let project = await Project.findOne({ _id: req.params.projectId })
+    .populate("members", "name")
+    .populate("main_tw_contact", "name");
+  res.status(200).json(project);
+});
+
 router.get("/", async (req, res) => {
   let projects = await Project.find({}, "-description")
     .populate("members", "name")
     .populate("main_tw_contact", "name");
-  res.status(200).json({
-    projects
-  });
+  res.status(200).json(projects);
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,15 @@
 const User = require("../schemas/User");
 const router = require("express").Router();
 
+router.get("/:userId", async (req, res) => {
+  let user = await User.findOne({ _id: req.params.userId }).populate(
+    "projects",
+    "name"
+  );
+  res.status(200).json(user);
+});
 router.get("/", async (req, res) => {
   let users = await User.find({}).populate("projects", "name");
-  res.status(200).json({ users });
+  res.status(200).json(users);
 });
 module.exports = router;
