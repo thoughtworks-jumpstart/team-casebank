64040de795ca223e1d301b8c9bd4e6753cf0a6f6
Brian <tjtbrian@gmail.com>
2018-11-21 14:26:01 +0800

    [Brian] Set environment for seed

--- server/seed.js
+++ server/seed.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: process.env.MONGODB_URI,
-  testing: process.env.MONGODB_URI,
+  auto: process.env.MONGODB_URI,
+  test: process.env.MONGODB_URI,
   production: process.env.MONGODB_URI
 };
