cb1ed44a6cc12b3de01d63402bb5f4522736d98b
Brian <tjtbrian@gmail.com>
2018-11-21 14:33:52 +0800

    [Brian] changed seed env name

--- server/seed.js
+++ server/seed.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: process.env.MONGODB_URI,
-  auto: process.env.MONGODB_URI,
+  automation: process.env.MONGODB_URI,
   test: process.env.MONGODB_URI,
   production: process.env.MONGODB_URI
 };
