bcba174c5c30bfa9c988906a3bc9d4d6fd210757
“newzealot” <github@newzealot.com>
2018-11-22 16:40:28 +0800

    [Kaixin/Calvin] Refine logo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { toggle } from "../../utils/toggle";
+import logo from "../../logo.png";
 
 export default class NavBar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand " to="/">
-          ProjectHub
+          <img src={logo} alt="logo of Owl" width="80%" height="80%" />
         </NavLink>
         <button
           className="navbar-toggler"
