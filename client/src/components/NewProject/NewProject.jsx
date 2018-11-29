b1bd1a529f66d6e5a8a542f3af81cf092eb6e0f5
ChuaKaixin <chuakaixin.working@gmail.com>
2018-11-29 14:59:34 +0800

    [Sheldon/Kaixin] #37 return to home page if the person logout in add project page

--- client/src/components/NewProject/NewProject.jsx
+++ client/src/components/NewProject/NewProject.jsx
@@ -6,6 +6,7 @@ import { getAttributes, create } from "../../data/attributeService";
 import { getUsers } from "../../data/userService";
 import { createProject } from "../../data/projectService";
 import { Redirect } from "react-router-dom";
+import { AuthConsumer } from "../UserDetails/AuthContext";
 export default class NewProject extends Component {
   constructor(props) {
     super(props);
@@ -98,44 +99,56 @@ export default class NewProject extends Component {
 
   render() {
     let attributes = this.state.attributes;
+
     if (this.state.created) {
       return <Redirect to={`/results/details/${this.state.response._id}`} />;
     }
     return attributes.length ? (
-      <div className="container-fluid p-4">
-        <div className="row">
-          <div className="col">
-            <Title
-              clients={
-                this.state.attributes.filter(a => a.attribute === "Client")[0]
-              }
-              selected={this.state.selectedOptions.Client}
-              onChange={this.updateAttributes}
-              submit={this.submitProject}
-              name={this.state.name}
-              saveName={this.saveName}
-              createOption={this.createOption}
-            />
+      <AuthConsumer>
+        {({ isAuth }) => (
+          <div>
+            {isAuth && (
+              <div className="container-fluid p-4">
+                <div className="row">
+                  <div className="col">
+                    <Title
+                      clients={
+                        this.state.attributes.filter(
+                          a => a.attribute === "Client"
+                        )[0]
+                      }
+                      selected={this.state.selectedOptions.Client}
+                      onChange={this.updateAttributes}
+                      submit={this.submitProject}
+                      name={this.state.name}
+                      saveName={this.saveName}
+                      createOption={this.createOption}
+                    />
+                  </div>
+                </div>
+                <div className="row m-4">
+                  <div className="col-3 pl-4">
+                    <ProjectAttributes
+                      onChange={this.updateAttributes}
+                      attributes={attributes}
+                      selected={this.state.selectedOptions}
+                      region={this.state.selectedOptions.Region}
+                      createOption={this.createOption}
+                    />
+                  </div>
+                  <div className="col-9">
+                    <Editor
+                      onChangeHTML={this.onChangeHTML}
+                      value={this.state.content}
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+            {!isAuth && <Redirect to={"/"} />}
           </div>
-        </div>
-        <div className="row m-4">
-          <div className="col-3 pl-4">
-            <ProjectAttributes
-              onChange={this.updateAttributes}
-              attributes={attributes}
-              selected={this.state.selectedOptions}
-              region={this.state.selectedOptions.Region}
-              createOption={this.createOption}
-            />
-          </div>
-          <div className="col-9">
-            <Editor
-              onChangeHTML={this.onChangeHTML}
-              value={this.state.content}
-            />
-          </div>
-        </div>
-      </div>
+        )}
+      </AuthConsumer>
     ) : null;
   }
 }
